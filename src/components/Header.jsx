57d473e8a83877b54918b4a546a054203d97cda0
danielcassiano <danielcassianoxj6@gmail.com>
2026-03-20 16:56:01 -0400

    Criado o componente da Lincagem do Criar link no Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,28 @@ function Header() {
                 <span>🗂️</span>
                 <h1>Minha Coleção</h1>
             </div>
-            <nav className="header-nav">
-                <a href="/">Início</a>
-                <a href="/inventario">Inventário</a>
-            </nav>
+             <nav className="header-nav">
+        {/* NavLink recebe uma função em className */}
+        {/* isActive é true quando a URL atual combina com 'to' */}
+        <NavLink 
+          to="/" 
+          end
+          className={({ isActive }) => 
+            isActive ? 'nav-link ativo' : 'nav-link'
+          }
+        >
+          Início
+        </NavLink>
+        
+        <NavLink 
+          to="/inventario"
+          className={({ isActive }) => 
+            isActive ? 'nav-link ativo' : 'nav-link'
+          }
+        >
+          Inventário
+        </NavLink>
+      </nav>
         </header>
     )
 }
